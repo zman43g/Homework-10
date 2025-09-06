d5def35345c5a71eedc1f930ba4fd2e503ea7afe
zman43g <zman3@yandex.ru>
2025-09-06 11:04:51 +0700

    HW 10

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        int arr[] = new int[]{1, 2, 3};
+        System.out.println(Arrays.toString(arr));
+        float arr2[] = {1.57F, 7.654F, 9.986F};
+        System.out.println(Arrays.toString(arr2));
+        double arr3[] = {2.853,769892.2921949234, 25289512.58823832934, 8888.9999999};
+        System.out.println(Arrays.toString(arr3));
+
+
+    }
+}
